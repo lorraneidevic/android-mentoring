0c0ac19d7f1c4739aaea7400053f25186a0dbf65
lorraneidevic <lidevic@thoughtworks.com>
2018-08-01 17:56:14 -0300

    Testing android VCS

--- app/src/androidTest/java/com/example/lorraneidevic/flavinharainhaeorestonadinha/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/lorraneidevic/flavinharainhaeorestonadinha/ExampleInstrumentedTest.java
@@ -2,13 +2,13 @@ package com.example.lorraneidevic.flavinharainhaeorestonadinha;
 
 import android.content.Context;
 
-import androidx.test.InstrumentationRegistry;
-import androidx.test.runner.AndroidJUnit4;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import androidx.test.InstrumentationRegistry;
+import androidx.test.runner.AndroidJUnit4;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -19,7 +19,7 @@ import static org.junit.Assert.*;
 public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() {
-        // Context of the app under test.
+        // Context of the app under test..
         Context appContext = InstrumentationRegistry.getTargetContext();
 
         assertEquals("com.example.lorraneidevic.flavinharainhaeorestonadinha", appContext.getPackageName());
